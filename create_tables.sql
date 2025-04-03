f75b607d8d409582d320b4ecfbd1edeeda698823
philipdsijerkovic <122295715+philipdsijerkovic@users.noreply.github.com>
2025-04-02 23:33:59 -0400

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(25) NOT NULL,
@@ -9,30 +9,30 @@ CREATE TABLE users (
 );
 
 CREATE TABLE categories (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(50) NOT NULL
+    categories_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    categories_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE products (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(50) NOT NULL,
-    description TEXT,
-    image_url VARCHAR(400),
-    price DECIMAL(10, 2) NOT NULL,
-    category_id INTEGER NOT NULL,
-    featured BOOLEAN DEFAULT FALSE
+    products_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    products_name VARCHAR(50) NOT NULL,
+    products_description TEXT,
+    products_image_url VARCHAR(400),
+    products_price DECIMAL(10, 2) NOT NULL,
+    products_category_id INTEGER NOT NULL,
+    featured BOOLEAN DEFAULT FALSE,
 );
 
 CREATE TABLE carts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     status TEXT CHECK(status IN ('new', 'abandoned', 'purchased')) DEFAULT 'new',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL,
 );
 
 CREATE TABLE cart_products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     cart_id INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL CHECK (quantity > 0)
+    quantity INTEGER NOT NULL,
 );
